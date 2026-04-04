5297a32fca1c4ea861724439c0cfb0bc98668a6e
Diego Villamizar <42784004+craidol2@users.noreply.github.com>
2026-04-04 15:17:14 -0500

    hello world funcionando

--- src/main/java/com/example/jbpm/HelloWorldWorkItemHandler.java
+++ src/main/java/com/example/jbpm/HelloWorldWorkItemHandler.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.jbpm;
 
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
@@ -8,6 +8,10 @@ public class HelloWorldWorkItemHandler implements WorkItemHandler {
 
 
     public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+        String serviceInterface = (String) workItem.getParameter("Interface");
+        String serviceOperation = (String) workItem.getParameter("Operation");
+        System.out.println("Executing interface: " + serviceInterface + ", with operation: " + serviceOperation);
+
         System.out.println("HelloWorldWorkItemHandler: executeWorkItem: Hello World!");
     }
 
